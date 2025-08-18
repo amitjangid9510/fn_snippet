e7bf9f38a0806651228098bd02d7650cfe2ff984
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-08-18 13:16:58 +0530

    Create PrivateRoute.jsx

---
+++ PrivateRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+import { getCurrentUser } from "../utils/auth";
+
+const PrivateRoute = ({ children }) => {
+  const user = getCurrentUser();
+  return user ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
