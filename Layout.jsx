eacdb9e65e27ab46d2e3ae849bfd82b3fb774b5c
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-08-19 19:59:15 +0530

    Update Layout.jsx

--- Layout.jsx
+++ Layout.jsx
@@ -1,7 +1,6 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import { motion } from 'framer-motion';
 
 const Layout = () => {
   const location = useLocation();
@@ -10,23 +9,13 @@ const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-secondary-DEFAULT">
       <Header />
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className="flex-grow"
-      >
         <Outlet />
-      </motion.main>
       {!hideFooter && <Footer />}
     </div>
   );
 };
 
 export default Layout;
-
-
-
 
 
 // import { Outlet } from 'react-router-dom';
