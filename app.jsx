81c1ffc7309088847d482132df05bf8d42a73e0e
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-08-19 19:22:32 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -7,27 +7,15 @@ import PrivateRoute from './components/PrivateRoute';
 
 // user pages
 import Home from "./pages/user/Home";
-import Contact from "./pages/user/Contact";
-import About from "./pages/user/About";
-import Login from "./pages/user/Login";
-import Signup from "./pages/user/Signup";
 import Products from "./pages/user/Products";
 import ProductDetail from "./pages/user/ProductDetail";
 import NotFound from "./pages/user/NotFound";
 import UserProfile from "./pages/user/UserProfile";
-import Checkout from "./pages/user/Checkout";
-import Wishlist from "./pages/user/Wishlist";
-import Account from "./pages/user/Account";
 
 // admin pages
 import AdminLogin from "./pages/admin/Login";
 import AdminLayout from "./components/admin/Layout";
-import AdminProducts from "./pages/admin/UploadProduct";
-import AdminUsers from "./pages/admin/Users";
-import AdminSettings from "./pages/admin/Account";
 import AdminDashbord from "./pages/admin/Dashbord";
-import AdminSales from "./pages/admin/Sales";
-import ProductList from "./pages/admin/ProductList";
 import ProductUpdate from "./pages/admin/ProductUpdate";
 
 function App() {
@@ -49,16 +37,9 @@ return (
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="products" element={<PrivateRoute><Products /></PrivateRoute>}} />
+          <Route path="products" element={<PrivateRoute><Products /></PrivateRoute>} />
           <Route path="products/:id" element={<ProductDetail />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
           <Route path="user-profile" element={<UserProfile />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="wishlist" element={<Wishlist />} />
-          <Route path="account" element={<Account />} />
           <Route path="*" element={<NotFound />} />
         </Route>
 
@@ -66,11 +47,6 @@ return (
 
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminDashbord />} />
-          <Route path="products" element={<AdminProducts />} />
-          <Route path="users" element={<AdminUsers />} />
-          <Route path="sales" element={<AdminSales />} />
-          <Route path="settings" element={<AdminSettings />} />
-          <Route path="product-list" element={<ProductList />} />
           <Route path="product-update/:id" element={<ProductUpdate />} />
           <Route path="*" element={<NotFound />} />
         </Route>
@@ -80,4 +56,3 @@ return (
 }
 
 export default App;
-    
