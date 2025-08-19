69f2caf5d3558120263e68b5cd24d11c496cc64f
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-08-19 19:36:12 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,7 +3,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { store, persistor } from './app/store.jsx';
+import { store, persistor } from './store/store.jsx';
 import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
