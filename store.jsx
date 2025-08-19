488e6eca2b697dbe05c2a3bfb0d01d8531a9dcdd
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-08-19 19:33:21 +0530

    Update store.jsx

--- store.jsx
+++ store.jsx
@@ -1,26 +1,16 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import adminAuthReducer from '../features/auth/adminAuth';
-import adminProductReducer from '../features/admin/productSlice';
-import productReducer from '../features/user/productSlice';
-import cartReducer from '../features/user/cartSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import usersReducer from '../features/admin/usersSlice';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth', 'cart'], //states will persist
+  whitelist: ['auth'], //states will persist
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  adminAuth : adminAuthReducer,
-  adminProducts: adminProductReducer,
-  cart: cartReducer,
-  users: usersReducer,
-  products: productReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -34,6 +24,5 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
-
 
 
