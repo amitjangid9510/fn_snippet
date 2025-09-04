542c8a79ad0582c8bf7932b066338f312a42400d
Amit jangid <56021794+amitjangid9510@users.noreply.github.com>
2025-09-04 18:58:46 +0530

    Update vitconfig_for_shadcn.js

--- vitconfig_for_shadcn.js
+++ vitconfig_for_shadcn.js
@@ -38,28 +38,28 @@ export default defineConfig({
 //  npm install react-router-dom axios @reduxjs/toolkit redux-persist react-redux
 //  npm install react-hook-form @hookform/resolvers yup
 
-
 //--------------# Windows ---------------
-//mkdir src\pages
-//mkdir src\layout
-//mkdir src\utils
-//mkdir src\store
-//mkdir src\store\features
-//mkdir src\validations
-
-//New-Item -Path src\store\store.js -ItemType File
-//New-Item -Path src\layout\Layout.jsx -ItemType File
-//New-Item -Path src\validations\SchemaValidation.js -ItemType File
-//New-Item -Path src\utils\api.js -ItemType File
+// mkdir src\pages
+// mkdir src\layout
+// mkdir src\utils
+// mkdir src\store
+// mkdir src\store\features
+// mkdir src\validations
+// New-Item -Path src\store\store.js -ItemType File
+// New-Item -Path src\layout\Layout.jsx -ItemType File
+// New-Item -Path src\validations\SchemaValidation.js -ItemType File
+// New-Item -Path src\utils\api.js -ItemType File
+// New-Item -Path src\store\features\userSlice.js -ItemType File
+// New-Item -Path src\pages\Home.jsx -ItemType File
 
 //--------------# linux ---------------
-
 //mkdir -p src/pages src/layout src/utils src/store/features src/validations
-
 //touch src/store/store.js
 //touch src/layout/Layout.jsx
 //touch src/validations/SchemaValidation.js
 //touch src/utils/api.js
+//touch src\store\features\userSlice.js
+//touch src\pages\Home.jsx
 
 
 
